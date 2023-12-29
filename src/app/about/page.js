272e1a1fc4bb95454139f80796913f7f110961f6
Tapple214 <111862212+Tapple214@users.noreply.github.com>
2023-12-29 21:01:41 +0800

    About page

---
+++ src/app/about/page.js
@@ -0,0 +1,45 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowLeft } from 'react-feather';
+import './aboutPage.css'
+
+export default function AboutPage() {
+    return (
+        <>
+            <Row className='p-3 sticky-top bg-white w-auto'>
+                <Col xs='12' lg='12' className='d-flex'>
+                    <ArrowLeft />
+                    <h1 className='w-100 text-center me-4 fw-medium'>Help Centre</h1>
+                </Col>
+            </Row>
+            
+            <Container>
+                <Row className='mt-2 mx-auto d-flex justify-content-center'>
+                    <Col xs='12' lg='5'>
+                        <h1>Lorem ipsum</h1>
+                        <p></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Col>
+                </Row>
+
+                <Row className='mt-2 mx-auto d-flex justify-content-center'>
+                    <Col xs='12' lg='5'>
+                        <h1>Lorem ipsum</h1>
+                        <p></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Col>
+                </Row>
+
+                <Row className='mt-2 mx-auto d-flex justify-content-center'>
+                    <Col xs='12' lg='5'>
+                        <h1>Lorem ipsum</h1>
+                        <p></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
